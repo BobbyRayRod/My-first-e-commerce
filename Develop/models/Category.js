e7bb3f0d1981495030a768337fbb62f2bb052a2a
Bobby <bobbyrayrod92@gmail.com>
2022-12-14 15:33:39 -0700

    added .env file to start seeding

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,16 +1,26 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
-
 class Category extends Model {}
+
+const sequelize = require('../config/connection.js');
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     // define columns
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
